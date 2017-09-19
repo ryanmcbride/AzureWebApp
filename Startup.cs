2e4918bd3cc81e7a58dfdc9c666202947db669cd
Ryan McBride <ryanmcbride@Ryans-MacBook-Pro.local>
2017-09-18 20:52:13 -0600

    try sql server

--- Startup.cs
+++ Startup.cs
@@ -24,9 +24,16 @@ namespace AzureWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-
+#if USE_SQLITE_DB
             services.AddDbContext<MvcMovieContext>(options =>
             options.UseSqlite("Data Source=MvcMovie.db"));
+#else
+            services.AddDbContext<MvcMovieContext>(options =>
+                    {
+                        var connString = "Server=tcp:simplewebapp.database.windows.net,1433;Initial Catalog=MvcMovie;Persist Security Info=False;User ID=ryanmcbride;Password=Takeoff41;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                        options.UseSqlServer(connString);
+                    });
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
